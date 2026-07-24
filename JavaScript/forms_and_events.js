62cb30f56474c5c80412efb82f5e53e7d719a690
User <User@DESKTOP-QHG18FL>
2026-07-24 21:37:26 +0300

    Skin switch DONE

--- JavaScript/forms_and_events.js
+++ JavaScript/forms_and_events.js
@@ -70,11 +70,13 @@ function traceMouse(e)
 document.getElementById("switch-background").addEventListener("click", switchBackground);
 function switchBackground(e)
 {
+	document.body.className = document.body.className === "dark" ? "light" : "dark";
+	/*
 	let skin = document.body.className;
 	let switchButton = document.getElementById("switch-background");
 	switchButton.src = skin === "dark" ? "moon.png" : "sun.png";
 	document.body.className = skin === "dark" ? "light" : "dark";
 	//document.getElementById("debug-background").innerHTML = switchButton.src;
 	document.getElementById("debug-background").innerHTML = document.body.className;
-
+	*/
 }
